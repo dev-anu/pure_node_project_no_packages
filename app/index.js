50ba228100dc64cea236e8db1b098c354495d9d2
shweta388 <shwetasahay449@gmail.com>
2021-02-13 00:54:45 +0530

    read req using url lib

---
+++ app/index.js
@@ -0,0 +1,19 @@
+/*
+ * Primary file for the API
+ *
+ */
+
+var http = require("http");
+var url = require("url");
+
+var server = http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url, true);
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  res.end("Hello World\n");
+  console.log("request is received on this path " + trimmedPath);
+});
+
+server.listen(3000, function () {
+  console.log("The server is listening on port 3000");
+});
